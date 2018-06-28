e066197817a2ad3896fa3512d6d7bba118655648
emanueng <emanuelhbga@gmail.com>
2018-06-27 21:48:45 -0300

    reduzir numero de parametros

--- taskbot.py
+++ taskbot.py
@@ -9,75 +9,35 @@ import sqlalchemy
 
 import db
 from db import Task
-
-def lerarq():
-    arq=open('TOKEN.txt','r')
-    texto=arq.read()
-    texto2=texto.strip()
-    arq.close()
-    return texto2
-    
-TOKEN = lerarq()
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-print(URL)
-
-HELP = """
- /new NOME
- /todo ID
- /doing ID
- /done ID
- /delete ID
- /list
- /rename ID NOME
- /dependson ID ID...
- /duplicate ID
- /priority ID PRIORITY{low, medium, high}
- /help
-"""
-def getdata(msg):
-    parts=msg.split() 
+class Funcoes():
+  def __init__(self,chat,msg):
+     self.chat=chat
+     self.msg=msg
+  def setmsg(self,msg):
+   self.msg=msg
+  def getdata(self):
+    parts=self.msg.split() 
     if(parts!=[]):
       data=parts[0]
     else:
       data="0"
     return data 
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
 
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
-
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += "&offset={}".format(offset)
-    print (url)
-    js = get_json_from_url(url)
-    print (js)
-    return js
-
-def send_message(text, chat_id, reply_markup=None):
+  def send_message(self,game,text, reply_markup=None):  
+    chat_id=self.chat
     text = urllib.parse.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    url = game.URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
     if reply_markup:
         url += "&reply_markup={}".format(reply_markup)
-    get_url(url)
+    game.get_url(url)
 
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
 
-    return max(update_ids)
 
-def deps_text(task, chat, preceed=''):
+  def deps_text(self,task, preceed=''):
     text = ''
-
-    for i in range(len(task.dependencies.split(',')[:-1])):
+    chat=self.chat    
+    if(len(task.dependencies.split(',')[:-1])>0):
+      for i in range(len(task.dependencies.split(',')[:-1])):
         line = preceed
         query = db.session.query(Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
         dep = query.one()
@@ -97,63 +57,72 @@ def deps_text(task, chat, preceed=''):
 
         text += line
 
-    return text
-def error(msg,chat):
+      return text
+  def error(self,game):
     task_id=0;
+    msg=self.msg
     if not msg.isdigit():
-       send_message("You must inform the task id", chat) 
+       self.send_message(game,"You must inform the task id") 
     else:
        task_id = int(msg)
     return task_id;
-def lookupbank(task_id,chat):
+  def lookupbankt(self,task_id,game):
+      if(self.lookupbank(task_id,game)):
+           query = db.session.query(Task).filter_by(id=task_id, chat=self.chat) 
+           task = query.one()
+           return task
+  def lookupbank(self,task_id,game):
+    chat=self.chat
     try:
       query = db.session.query(Task).filter_by(id=task_id, chat=chat) 
       task = query.one()
       return True  
     except sqlalchemy.orm.exc.NoResultFound:
-      send_message("_404_ Task {} not found x.x".format(task_id), chat)
+      self.send_message(game,"_404_ Task {} not found x.x".format(task_id))
       return False
-def isvalid(msg,chat):
-    task_id=error(msg,chat)
+  def isvalid(self,game):
+    task_id=self.error(game)
     if(task_id>0):
-      return lookupbank(task_id,chat)     
+      return self.lookupbank(task_id,game)     
     else:
       return False    
-def status(msg,chat, status, print2):
-     if(isvalid(msg,chat)):
-       task_id=int(msg)
+  def status(self,game, status, print2):
+     chat=self.chat
+     if(self.isvalid(game)):
+       task_id=int(self.msg)
        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
        task=query.one()     
        task.status = status 
        db.session.commit() 
-       send_message(print2.format(task.id, task.name), chat)     
-def statusinform(command,msg,chat):
-        numbers=msg.split()
+       self.send_message(game,print2.format(task.id, task.name))     
+  def statusinform(self,command,game):
+        numbers=self.msg.split()
         for number in numbers: 
           if command == '/todo':
-             status(number,chat,'TODO',"*TODO* task [[{}]] {}")     
+             self.status(game,'TODO',"*TODO* task [[{}]] {}")     
           elif command == '/doing':
-             status(number,chat,'DOING',"*DOING* task [[{}]] {}")   
+             self.status(game,'DOING',"*DOING* task [[{}]] {}")   
           elif command == '/done':
-              status(number,chat,'DONE',"*DONE* task [[{}]] {}")    
-def printdata(data,id,chat):
+             self.status(game,'DONE',"*DONE* task [[{}]] {}")    
+  def printdata(self,data,id,game):
     if (id  in data.keys()):
-      send_message(data[id],chat) 
+      send_message(data[id],self.chat) 
 # a variavel que armazena informações a serem impressas na tela, chat informa o local as informações serão impressas, data representa a data de netrega , text a palavra usada para filtrar as tasks, text2 o simbolo que sera mostrado no inicio do filtro das palavras. 
-def printforstatus(a,chat,data, text,text2):
+  def printforstatus(self,a,game,data, text,text2):
     a+=text2
-    query = db.session.query(Task).filter_by(status=text, chat=chat).order_by(Task.id)
-    return printafor(a,query,chat,data)    
-def printafor(a,query,chat,data):
+    query = db.session.query(Task).filter_by(status=text, chat=self.chat).order_by(Task.id)
+    return self.printafor(a,query,data,game)    
+  def printafor(self,a,query,data,game):
     for task in query.all():
                 a += '[[{}]] {}\n'.format(task.id, task.name)
-                printdata(data,task.id,chat)
+                self.printdata(data,task.id,game)
     return a
-def printforpriority(a,chat,data, text,text2):
+  def printforpriority(self,a,game,data, text,text2):
     a+=text2
-    query = db.session.query(Task).filter_by(priority=text, chat=chat).order_by(Task.id)
-    return printafor(a,query,chat,data)                              
-def list(chat,msg,data):
+    query = db.session.query(Task).filter_by(priority=text, chat=self.chat).order_by(Task.id)
+    return self.printafor(a,query,data,game)                              
+  def list(self,game,data):
+            chat=self.chat
             a = ''
             a += '\U0001F4CB Task List\n'
             query = db.session.query(Task).filter_by(parents='', chat=chat).order_by(Task.id)
@@ -165,47 +134,53 @@ def list(chat,msg,data):
                     icon = '\U00002611'
 
                 a += '[[{}]] {} {}\n'.format(task.id, icon, task.name)
-                a += deps_text(task, chat)
+                a += self.deps_text(task)
 
-            send_message(a, chat)
+            self.send_message(a, chat)
             a = ''
             
             a += '\U0001F4DD _Status_\n'
-            a= printforstatus(a,chat,data,'TODO','\n\U0001F195 *TODO*\n')
-            a= printforstatus(a,chat,data,'DOING','\n\U000023FA *DOING*\n')
-            a= printforstatus(a,chat,data,'DONE','\n\U00002611 *DONE*\n')
-            a= printforpriority(a,chat,data,'low','\n\U00002611 *LOW*\n') 
-            a=printforpriority(a,chat,data,'medium', '\n\U000023FA *MEDIUM*\n')   
-            a=printforpriority(a,chat,data,'high','\n\U000023FA *HIGH*\n')
-            send_message(a, chat)              
-def priority(chat,msg):
+            a= self.printforstatus(a,game,data,'TODO','\n\U0001F195 *TODO*\n')
+            a= self.printforstatus(a,game,data,'DOING','\n\U000023FA *DOING*\n')
+            a= self.printforstatus(a,game,data,'DONE','\n\U00002611 *DONE*\n')
+            a= self.printforpriority(a,game,data,'low','\n\U00002611 *LOW*\n') 
+            a=self.printforpriority(a,game,data,'medium', '\n\U000023FA *MEDIUM*\n')   
+            a=self.printforpriority(a,game,data,'high','\n\U000023FA *HIGH*\n')
+            self.send_message(game,a) 
+  def separamsg(self):
+       msg=self.msg   
+       if len(msg.split(' ', 1)) > 1:
+            text = msg.split(' ', 1)[1]
+       msg = msg.split(' ', 1)[0]
+       self.setmsg(msg)
+       return text                 
+  def priority(self,game):
             text = ''
+            msg=self.msg
             if msg != '':
-                if len(msg.split(' ', 1)) > 1:
-                    text = msg.split(' ', 1)[1]
-                msg = msg.split(' ', 1)[0]
-
-            task_id=error(msg,chat)
-            if(isvalid(msg,chat)):
+                text=self.separamsg()
+            
+            msg=self.msg
+            if(self.isvalid(game)):
                task_id=int(msg)
-               query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+               query = db.session.query(Task).filter_by(id=task_id, chat=self.chat)
                task=query.one()
             else:
                return
             if text == '':
                task.priority = ''
-               send_message("_Cleared_ all priorities from task {}".format(task_id), chat)
+               self.send_message(game,"_Cleared_ all priorities from task {}".format(task_id))
             else:
                if text.lower() not in ['high', 'medium', 'low']:
-                 send_message("The priority *must be* one of the following: high, medium, low", chat)
+                 self.send_message("The priority *must be* one of the following: high, medium, low", chat)
                else:
                  task.priority = text.lower()
-                 send_message("*Task {}* priority has priority *{}*".format(task_id, text.lower()), chat)
+                 self.send_message(game,"*Task {}* priority has priority *{}*".format(task_id, text.lower()))
                  db.session.commit()
-def duplicate(chat,msg):  
-             if(isvalid(msg,chat)):
-                  task_id=int(msg)
-                  query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+  def duplicate(self,game):  
+             if(self.isvalid(game)):
+                  task_id=int(self.msg)
+                  query = db.session.query(Task).filter_by(id=task_id, chat=self.chat)
                   task=query.one()
              else:
                   return
@@ -218,44 +193,89 @@ def duplicate(chat,msg):
                 t.parents += '{},'.format(dtask.id)
 
              db.session.commit()
-             send_message("New task *TODO* [[{}]] {}".format(dtask.id, dtask.name), chat)
-
-def rename(chat,msg):
+             self.send_message(game,"New task *TODO* [[{}]] {}".format(dtask.id, dtask.name))
+  
+  def rename(self,game):
             text = ''
+            msg=self.msg
             if msg != '':
-                if len(msg.split(' ', 1)) > 1:
-                    text = msg.split(' ', 1)[1]
-                msg = msg.split(' ', 1)[0]
-            if(isvalid(msg,chat)):
+                  text=self.separamsg() 
+            msg=self.msg      
+            if(self.isvalid(game)):
                   task_id=int(msg)
-                  query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+                  query = db.session.query(Task).filter_by(id=task_id, chat=self.chat)
                   task=query.one()
             else:
                   return  
             if text == '':
-               send_message("You want to modify task {}, but you didn't provide any new text".format(task_id), chat)
+               send_message(game,"You want to modify task {}, but you didn't provide any new text".format(task_id))
                return
 
             old_text = task.name
             task.name = text
             db.session.commit()
-            send_message("Task {} redefined from {} to {}".format(task_id, old_text, text), chat)
+            self.send_message(game,"Task {} redefined from {} to {}".format(task_id, old_text, text))
 
-def delete(chat,msg):
-               if(isvalid(msg,chat)):
-                  task_id=int(msg)
-                  query = db.session.query(Task).filter_by(id=task_id, chat=chat)
-                  task=query.one()
+  def delete(self,game):
+               if(self.isvalid(game)):
+                  task_id=int(self.msg)
+                  task=self.lookupbankt(task_id,game)
                else:
                   return
                for t in task.dependencies.split(',')[:-1]:
-                    qy = db.session.query(Task).filter_by(id=int(t), chat=chat)
-                    t = qy.one()
+                    t=self.lookupbankt(int(t),game)
                     t.parents = t.parents.replace('{},'.format(task.id), '')
                db.session.delete(task)
                db.session.commit()
-               send_message("Task [[{}]] deleted".format(task_id), chat)       
-def handle_updates(updates):
+               self.send_message(game,"Task [[{}]] deleted".format(task_id))   
+class Game():
+  
+  def lerarq(self):
+    arq=open('TOKEN.txt','r')
+    texto=arq.read()
+    texto2=texto.strip()
+    arq.close()
+    return texto2
+    
+  
+ 
+
+  HELP = """
+   /new NOME
+   /todo ID
+   /doing ID
+   /done ID
+   /delete ID
+   /list
+   /rename ID NOME
+   /dependson ID ID...
+   /duplicate ID
+   /priority ID PRIORITY{low, medium, high}
+   /help
+   """
+  def __init__(self):
+      TOKEN = self.lerarq()   
+      self.URL = "https://api.telegram.org/bot{}/".format(TOKEN) 
+  def get_url(self,url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+  def get_json_from_url(self,url):
+    content = self.get_url(url)
+    js = json.loads(content)
+    return js
+  def setmsg(self,msg):
+      self.msg=msg
+  def get_updates(self,offset=None):
+    url = self.URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    print (url)
+    js = self.get_json_from_url(url)
+    print (js)
+    return js
+  def handle_updates(self,updates):
     for update in updates["result"]:
         if 'message' in update:
             message = update['message']
@@ -271,36 +291,35 @@ def handle_updates(updates):
             msg = message["text"].split(" ", 1)[1].strip()
 
         chat = message["chat"]["id"]
-
-        print(command, msg, chat)
+        funcao1=Funcoes(chat,msg)
         data={}
         if command == '/new':
             task = Task(chat=chat, name=msg, status='TODO', dependencies='', parents='', priority='')
-            data2=getdata(msg)
+            data2=funcao1.getdata()
             data[task.id]=data2 
             db.session.add(task)
             db.session.commit()
-            send_message("New task *TODO* [[{}]] {}".format(task.id, task.name), chat)
+            funcao1.send_message(self,"New task *TODO* [[{}]] {}".format(task.id, task.name))
 
         elif command == '/rename':
-            rename(chat,msg)
+            funcao1.rename(self)
         elif command == '/duplicate':
-            duplicate(chat,msg)
+            funcao1.duplicate(self)
         elif command == '/delete':
-              delete(chat,msg)
+            funcao1.delete(self)
         elif(command=='/todo' or  command== '/doing' or command== '/done'):
-            statusinform(command,msg,chat)
+            funcao1.statusinform(command,self)
         elif command == '/list':
-             list(chat,msg,data)
+             funcao1.list(self,data)
         elif command == '/dependson':
             text = ''
+            
             if msg != '':
-                if len(msg.split(' ', 1)) > 1:
-                    text = msg.split(' ', 1)[1]
-                msg = msg.split(' ', 1)[0]
-
-            if(isvalid(msg,chat)):
-
+               text=funcao1.separamsg()
+            msg=funcao1.msg 
+            if(funcao1.isvalid(self)):
+                  task_id=int(msg)   
+                  task=funcao1.lookupbankt(msg,self)
                   if text == '':
                      for i in task.dependencies.split(',')[:-1]:
                          i = int(i)
@@ -313,13 +332,13 @@ def handle_updates(updates):
                   else:
                      for depid in text.split(' '):
                         if not depid.isdigit():
-                            send_message("All dependencies ids must be numeric, and not {}".format(depid), chat)
+                            self.send_message("All dependencies ids must be numeric, and not {}".format(depid), chat)
                         else:
                             depid = int(depid)
                             query = db.session.query(Task).filter_by(id=depid, chat=chat)
                             try:
                                 taskdep = query.one()
-                                taskdep.parents += str(task.id) + ','
+                                taskdep.parents += str(taskdep.id) + ','
                             except sqlalchemy.orm.exc.NoResultFound:
                                 send_message("_404_ Task {} not found x.x".format(depid), chat)
                                 continue
@@ -329,34 +348,43 @@ def handle_updates(updates):
                                 task.dependencies += str(depid) + ','
 
                   db.session.commit()
-                  send_message("Task {} dependencies up to date".format(task_id), chat)
+                  funcao1.send_message(self,"Task {} dependencies up to date".format(task_id))
             else:
               return 
         elif command == '/priority':
-           priority(chat,msg)
+            funcao1.priority(self)
         elif command == '/start':
-            send_message("Welcome! Here is a list of things you can do.", chat)
-            send_message(HELP, chat)
+            funcao.send_message(self,"Welcome! Here is a list of things you can do.")
+            funcao.send_message(game)
         elif command == '/help':
-            send_message("Here is a list of things you can do.", chat)
-            send_message(HELP, chat)
+            funcao.send_message(self,"Here is a list of things you can do.")
+            funcao.send_message(HELP, self)
         else:
-            send_message("I'm sorry dave. I'm afraid I can't do that.", chat)
+            funcao1.send_message(self,"I'm sorry dave. I'm afraid I can't do that." )
 
-def main():
+  def main(self):
     last_update_id = None
 
     while True:
         print("Updates")
-        updates = get_updates(last_update_id)
+        updates = self.get_updates(last_update_id)
 
         if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            handle_updates(updates)
+            last_update_id = self.get_last_update_id(updates) + 1
+            self.handle_updates(updates)
 
         time.sleep(0.5)
+  def get_last_update_id(self,updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+
+    return max(update_ids)
+
+    
 
 
 if __name__ == '__main__':
-    main()
+     game1=Game()
+     game1.main()
 
